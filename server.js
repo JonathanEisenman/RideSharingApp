be186cab306692bbe617d9892820f472698f549b
Anthony Sasso <89811415+SassoAnthony@users.noreply.github.com>
2022-12-02 14:40:27 -0500

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-
-const hostname = '0.0.0.0';
-const port = 3000;
-
-const server = require('./controller.js');
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
